581559ad6d664ff473dbd6664789dd4e22f56962
alefagi <iltu@libero.it>
2021-07-29 16:05:25 +0200

    added input to add new items

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ const app = new Vue({
   el: '#app',
   data: {
     toDos: ['Fare la spesa', 'Pagare assicurazione auto', 'Prenotare visita oculistica', 'Andare dal barbiere'],
-
+    newItem: '',
   },
   methods: {
     removeItem(index) {
       this.toDos.splice(index, 1);
+    },
+    addItem() {
+      this.toDos.push(this.newItem);
     }
   },
 });
