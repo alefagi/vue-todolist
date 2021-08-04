13cc153494e8b514c2e70389573927ccd3728910
alefagi <iltu@libero.it>
2021-08-04 10:16:45 +0200

    added toggle on items

--- js/script.js
+++ js/script.js
@@ -5,8 +5,27 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-    toDos: ['Fare la spesa', 'Pagare assicurazione auto', 'Prenotare visita oculistica', 'Andare dal barbiere'],
+    toDos: [
+      {
+        text: 'Fare la spesa',
+        done: false,
+      },
+      {
+        text: 'Pagare assicurazione auto',
+        done: false,
+      }, 
+      {
+        text: 'Prenotare visita oculistica',
+        done: false,
+      }, 
+      {
+        text: 'Andare dal barbiere',
+        done: false,
+      }
+    ],
     newItem: '',
+    showInput: false,
+    searchTerm: '',
   },
   methods: {
     removeItem(index) {
@@ -14,9 +33,38 @@ const app = new Vue({
     },
     addItem() {
       if(this.newItem.trim() !== '') {
-        this.toDos.push(this.newItem);
+        const newItem = {
+          text: this.newItem,
+          done: false
+        };
+        this.toDos.push(newItem);
+        this.toggleInput();
+      } 
+      else {
+        this.newItem = '';
       }
+    },
+    toggleInput() {
+      this.showInput = !this.showInput;
       this.newItem = '';
+    },
+    showItem(itemText) {
+      if(!this.searchTerm || this.searchTerm.trim() === '') {
+        return true;
+      }
+      return itemText.toLowerCase().includes(this.searchTerm.trim().toLowerCase());
+    },
+    toggleChecked(index) {
+      const updatedToDos = this.toDos.map((item, itemIndex) => {
+        if(itemIndex === index) {
+          item.done = !item.done;
+        }
+        return item;
+      });
+      this.toDos = updatedToDos;
+    },
+    isDone(index) {
+      return this.toDos[index].done;
     }
   },
 });
